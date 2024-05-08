39b94ff1668914628a4846f64d42259a45dbaa95
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2024-05-08 09:22:46 -0400

    Add styling

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,12 +5,21 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>beatmods3</title>
+        <title>QeatQods 3</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
+        <nav class="navbar bg-base-100">
+          <a class="btn btn-ghost text-xl">daisyUI</a>
+        </nav>
         <Component />
       </body>
+      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+        <aside className="items-center grid-flow-row prose">
+          <p class={"italic"}>Pronounced BeatMods 3!</p>
+          <p>Copyright © 2024 - All right reserved</p>
+        </aside>
+      </footer>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 import { type Config } from "tailwindcss";
+import daisyui from "daisyui"
+import typography from "tailwindcss/typography"
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
+  ],
+  plugins: [
+    typography,
+    daisyui as any,
   ],
 } satisfies Config;
