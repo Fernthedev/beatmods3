27568e90f6f830f7282a84729868e5e6c4798e6b
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2024-05-10 00:51:31 -0400

    FIx home and error page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,26 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import { getVersions } from "./api/repo/index.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { PackageMetadata } from "../types.ts";
+import { getPackagesInVersion } from "./api/repo/[version]/index.ts";
 
-export default function Home() {
-  const count = useSignal(3);
+type HomePageData = {
+  versions: string[];
+};
+
+export const handler: Handlers<HomePageData> = {
+  async GET(_req, ctx) {
+    const versions = await getVersions();
+
+    return ctx.render({ versions });
+  },
+};
+
+export default function Home(props: PageProps<HomePageData>) {
+  const { versions } = props.data;
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 mx-auto bg-dark-200">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
@@ -13,12 +29,14 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+        <h1 class="text-4xl font-bold">Welcome to QeatMods 3</h1>
+        <div class="my-4 flex flex-col gap-2">
+          {versions.map((version) => (
+            <a class={"link prose text-lg"} href={`/repo/${version}`}>
+              {version}
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
